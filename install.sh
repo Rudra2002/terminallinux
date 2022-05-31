131570ac5482a3c4db4cf7349bcdbbe5234380d9
Rudra2002 <59008344+Rudra2002@users.noreply.github.com>
2022-06-01 03:16:20 +0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+sudo apt update && sudo apt install zathura -y
+cp news ~/.local/bin/news
+chmod +x ~/.local/bin/news
+
+cp bnews ~/.local/bin/bnews
+chmod +x ~/.local/bin/bnews
+
+cp onews ~/.local/bin/onews
+chmod +x ~/.local/bin/onews
+
